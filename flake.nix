4044fc8d64b57bd71cff7e9f3e1c1ead29045c0c
a14u <aliemre2002@gmail.com>
2024-04-14 14:58:06 +0300

    add flake.nix file

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A Nix-flake-based python development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.11-darwin";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            (python3.withPackages (p: (with p; [
+              python-lsp-server
+              python-lsp-ruff
+            ])))
+          ];
+        };
+      });
+    };
+}

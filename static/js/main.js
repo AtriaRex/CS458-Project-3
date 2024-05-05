5988ecf2ad53cc052561aa39505c195fa089202a
a14u <aliemre2002@gmail.com>
2024-05-05 14:06:26 +0300

    implement findNearestSea

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,25 @@ function calculateDistance(point1, point2) {
     return Math.sqrt(x * x + y * y);
 }
 
+const SEA_COORDS = {
+    "BlackSea": { x: 43.4130, y: 34.2993 },
+    "MarmaraSea": { x: 40.6681, y: 28.1123 },
+    "AegeanSea": { x: 39.0192, y: 25.2686 },
+    "MediterraneanSea": { x: 34.5531, y: 18.0480 },
+};
+
+function findNearestSea(position) {
+    let nearestSea = null;
+    for (const [sea, coords] of Object.entries(SEA_COORDS)) {
+        let distance = calculateDistance(position, coords);
+        if (!nearestSea || distance < nearestSea.distance) {
+            nearestSea = { sea, distance };
+        }
+    }
+
+    return nearestSea;
+}
+
 function coordinatesCallback(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
@@ -12,5 +31,6 @@ function coordinatesCallback(position) {
 
 module.exports = {
     calculateDistance,
-    coordinatesCallback
+    coordinatesCallback,
+    findNearestSea,
 };

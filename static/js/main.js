4f3eb6b53a58cb27a10f9991dc5e29a320d7ac15
a14u <aliemre2002@gmail.com>
2024-05-05 14:24:53 +0300

    fix the calculateDistance function

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,26 @@
 function calculateDistance(point1, point2) {
-    var x = point2.x - point1.x;
-    var y = point2.y - point1.y;
-    return Math.sqrt(x * x + y * y);
+    const R = 6371e3; // radius of earth in metres
+    const phi1 = point1.lat * Math.PI / 180;
+    const phi2 = point2.lat * Math.PI / 180;
+    const delta_phi = (point2.lat - point1.lat) * Math.PI / 180;
+    const delta_lambda = (point2.lon - point1.lon) * Math.PI / 180;
+
+    // Haversine formula
+    const a = Math.sin(delta_phi / 2) * Math.sin(delta_phi / 2) +
+        Math.cos(phi1) * Math.cos(phi2) *
+        Math.sin(delta_lambda / 2) * Math.sin(delta_lambda / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    const d = R * c; // in metres
+
+    return d;
 }
 
 const SEA_COORDS = {
-    "BlackSea": { x: 43.4130, y: 34.2993 },
-    "MarmaraSea": { x: 40.6681, y: 28.1123 },
-    "AegeanSea": { x: 39.0192, y: 25.2686 },
-    "MediterraneanSea": { x: 34.5531, y: 18.0480 },
+    "BlackSea": { lat: 43.4130, lon: 34.2993 },
+    "MarmaraSea": { lat: 40.6681, lon: 28.1123 },
+    "AegeanSea": { lat: 39.0192, lon: 25.2686 },
+    "MediterraneanSea": { lat: 34.5531, lon: 18.0480 },
 };
 
 function findNearestSea(position) {
@@ -26,6 +38,14 @@ function findNearestSea(position) {
 function coordinatesCallback(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
+
+    const nearestSea = findNearestSea({ lat: latitude, lon: longitude });
+    const nearestSeaText = document.querySelector("#nearestSea");
+    nearestSeaText.textContent = nearestSea.sea;
+
+    const nearestSeaDistance = document.querySelector("#nearestSeaDistance");
+    nearestSeaDistance.textContent = nearestSea.distance;
+
     console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
 };
 
--- static/js/main.test.js
+++ static/js/main.test.js
@@ -2,19 +2,12 @@ const { calculateDistance, findNearestSea } = require('./main');
 
 // calculateDistance tests
 test('distance between the same point should be zero', () => {
-    let point = { x: 1, y: 2 };
+    let point = { lat: 1, lon: 2 };
     expect(calculateDistance(point, point)).toBe(0);
-});
-
-test('distance between the (0, 0) and (4, 3) should be 5', () => {
-    let point0 = { x: 0, y: 0 };
-    let point1 = { x: 4, y: 3 };
-
-    expect(calculateDistance(point0, point1)).toBe(5);
 });
 
 // findNearestSea tests
 test('(43,34) should return BlackSea', () => {
-    let point = { x: 43, y: 34 };
+    let point = { lat: 43, lon: 34 };
     expect(findNearestSea(point).sea).toBe('BlackSea');
 });
